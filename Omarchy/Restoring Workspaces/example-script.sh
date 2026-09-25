54bf56349438aeb69260ed92cd6c516b49365bbc
Sebastian Fichtner <sebastian@nohype.ai>
2026-09-25 11:00:24 +0200

    Document how to automate workspace setup in Omarchy

---
+++ Omarchy/Restoring Workspaces/example-script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Example script to layout workspaces
+# Something like this could be placed in ~/.local/bin/layout-work
+
+hyprctl dispatch exec "[workspace 1 silent] nautilus $HOME/Projects"
+hyprctl dispatch exec "[workspace 2 silent] ghostty --working-directory=$HOME/Projects"
+hyprctl dispatch exec "[workspace 3 silent] code $HOME/Projects"
+hyprctl dispatch exec "[workspace 4 silent] brave-origin --new-window https://github.com"
+hyprctl dispatch exec "[workspace 4 silent] brave-origin --new-window https://docs.hypr.land"
+hyprctl dispatch exec "[workspace 5 silent] brave-origin --new-window https://mail.google.com"
+
+# switches you to workspace 1
+hyprctl dispatch workspace 1
